d69499ddf48a3af0c5304fb8a15f041a8d40909f
HANBYEOL-YOO-developer <yhb4ever@gmail.com>
2023-02-02 13:17:32 +0900

    Initial Commit

---
+++ PRTest/PRTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PRTestApp.swift
+//  PRTest
+//
+//  Created by HANBYEOL YOO on 2023/02/02.
+//
+
+import SwiftUI
+
+@main
+struct PRTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
